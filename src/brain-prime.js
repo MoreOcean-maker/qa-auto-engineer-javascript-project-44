7179c0edb59f8b3c4d26346c40ab575a398990b3
igor <moreworkshop320@gmail.com>
2024-12-05 23:07:56 +0300

    Рефакторинг игры prime  и добавление игрового движка в нее2

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -1,4 +1,4 @@
-console.log ('Answer "yes" if given number is prime. Otherwise answer "no".');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 // Функция для проверки простоты числа
 const isPrime = (number) => {
   if (number <= 1) {
